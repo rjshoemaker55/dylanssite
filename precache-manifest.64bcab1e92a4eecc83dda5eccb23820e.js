5810979b8ecc2ae88319701ddfdc6d8318db3b06
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-12-04 16:18:38 -0500

    Updates

---
+++ precache-manifest.64bcab1e92a4eecc83dda5eccb23820e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae156295926b3ca54e34d07ad24de2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1866511e4188e625e030",
+    "url": "/static/css/main.8b476731.chunk.css"
+  },
+  {
+    "revision": "92b52c4e673851385c81",
+    "url": "/static/js/2.736423bd.chunk.js"
+  },
+  {
+    "revision": "1866511e4188e625e030",
+    "url": "/static/js/main.1881d24c.chunk.js"
+  },
+  {
+    "revision": "b9a0d003f5cff5edc453",
+    "url": "/static/js/runtime-main.e041b31a.js"
+  }
+]);
