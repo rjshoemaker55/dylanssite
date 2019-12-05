8b5dff6e31d471e96aadb9b4cda9bef1d0972deb
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-12-04 20:37:24 -0500

    fixed basename for new repo name

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './scss/main.scss';
 
 import Landing from './screens/Landing';
+import Shop from './screens/Shop';
+import Blog from './screens/Blog';
+import Forum from './screens/Forum';
 import SideNav from './components/SideNav';
 import FloatingContactButton from './components/FloatingContactButton';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/survival-kits'>
       <SideNav />
       <div id='main-container'>
         <FloatingContactButton />
         <Switch>
           <Route exact path='/' component={Landing} />
+          <Route exact path='/shop' component={Shop} />
+          <Route exact path='/blog' component={Blog} />
+          <Route exact path='/forum' component={Forum} />
         </Switch>
       </div>
     </Router>
--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-const jQuery = require('jquery');
+import { Link } from 'react-router-dom';
 
 const SideNav = () => {
   const [navVisible, setNavVisible] = useState('block');
@@ -10,33 +10,24 @@ const SideNav = () => {
     console.log(navVisible);
   };
 
-  const animatedScroll = () => {
-    jQuery('#main-container').animate(
-      {
-        scrollTop: jQuery('#shop-display-wrapper').offset().top
-      },
-      600
-    );
-  };
-
   return (
     <div id='sidenav-wrapper'>
       <div id='sidenav-logo-wrapper'>
         <i className='fas fa-angle-double-up'></i>
       </div>
       <div id='sidenav-links-wrapper'>
-        <a href='!#' className='nav-link'>
+        <Link to='/' className='nav-link'>
           <i className='fas fa-home nav-icon'></i>
-        </a>
-        <div onClick={animatedScroll} className='nav-link'>
+        </Link>
+        <Link to='/shop' className='nav-link'>
           <i className='fas fa-shopping-bag nav-icon'></i>
-        </div>
-        <a href='!#' className='nav-link'>
+        </Link>
+        <Link to='/blog' className='nav-link'>
           <i className='fas fa-align-left nav-icon'></i>
-        </a>
-        <a href='!#' className='nav-link'>
+        </Link>
+        <Link to='/forum' className='nav-link'>
           <i className='fas fa-rss-square nav-icon'></i>
-        </a>
+        </Link>
         <div id='hide-nav-button' onClick={hideNav}>
           <i className='fas fa-arrow-circle-left'></i>
         </div>
